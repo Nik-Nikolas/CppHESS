d4237ac2330dd8b6670ee6caed611b51f9f2fc4a
Igor <igorlobanoff@gmail.com>
2024-01-10 19:36:28 +0300

    cosmetics

--- source/main.cpp
+++ source/main.cpp
@@ -12,7 +12,7 @@
 
 #include "GameFactory.h"
 
-// Encapsulated in class (as the private static) variables.
+// Encapsulated in class (as the private static) variables
 int32_t BoardGlobals::size_         = 0;
 int32_t BoardGlobals::longMoveStep_ = 0;
 int32_t BoardGlobals::delay_        = 0;
@@ -20,31 +20,31 @@ int32_t BoardGlobals::framesStep_   = START_FRAME;
 bool    BoardGlobals::isGlyphMode_  = true;
 int32_t King::counter_              = 0;
 
-// Singletons class members.
+// Singletons class members
 std::random_device* RandomDevice::rd_ = nullptr;
-std::mutex* MutexDevice::m_           = nullptr;
-__CONIO_H* __CONIO_H_INST::conio_     = nullptr;
+std::mutex* MutexDevice::m_           = nullptr; // Keyboard events semaphore
+__CONIO_H* __CONIO_H_INST::conio_     = nullptr; // Unix conio library impl
 
 
 int main(){
 
-  // Create a factory to build the main game entities.
+  // Create a factory to build the main game entities
   GameFactory factory;
   auto board = factory.makeBoard();
   auto console = factory.makeConsole( MutexDevice::getInstance() );
   auto game = factory.makeGame( board, console, MutexDevice::getInstance() );
 
-  // Keyboard events processing in a separate detached thread.
+  // Keyboard events processing in a separate detached thread
   console->start( board, game );
 
-  // Multiple game cycle.
+  // Multiple game cycle
   while( true ){
     try{
       game->start();
     }
     catch(...){
       std::cerr << std::endl << "Unknown error occured." << std::endl;
-      throw; // Abort the game.
+      throw; 
     }
 
     game->resetGame();
